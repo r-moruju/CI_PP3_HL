18090bab6e07506eb8a01ad5ac747344f6698c90
R Moruju <morujurazvan@gmail.com>
2022-09-21 17:55:18 +0000

    Set up connection to google worksheet

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 import random
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hight_low_game')
+
+acounts = SHEET.worksheet('acounts')
+print(acounts.get_all_values())
 
 countries = {
     "England": 60,
@@ -67,4 +82,4 @@ def main():
         game_on = get_answer()
 
 
-main()
+# main()
