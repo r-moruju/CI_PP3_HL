6a334ed8aec3ad8ac981a2f26338dec91003fbee
R Moruju <morujurazvan@gmail.com>
2022-09-22 20:03:50 +0000

    Add functions that login returning users

--- run.py
+++ run.py
@@ -16,9 +16,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hight_low_game')
 
-acounts = SHEET.worksheet('acounts')
-acounts_list = acounts.get_all_values()
-
+accounts = SHEET.worksheet('acounts')
+accounts_list = accounts.get_all_values()
+usernames = [lst[0] for lst in accounts_list]
 countries = {
     "England": 60,
     "Poland": 40,
@@ -55,7 +55,7 @@ def ask_for_username():
     while keep_looping:
         keep_looping = False
         input_name = input("Type username: ")
-        for lst in acounts_list:
+        for lst in accounts_list:
             if input_name == lst[0]:
                 print("This username already in use. Try a different one.")
                 keep_looping = True
@@ -71,11 +71,41 @@ def ask_for_passcode():
     return passcode
 
 
-def log_in():
+def log_in_username():
     """
     Ask user to login by entering username and passcode
     """
-    
+    while True:
+        existing_username = input("Enter your username: ")
+        if existing_username == "exit":
+            return False
+        elif existing_username not in usernames:
+            print(f"There is no account with the username {existing_username}")
+            print("You can try again or type 'exit' to close")
+        else:
+            print(f"Welcome back {existing_username}")
+            break
+    return existing_username
+
+
+def log_in_passcode(username):
+    """
+    Ask for curent user passcode and check if match
+    """
+    for lst in accounts_list:
+        if lst[0] == username:
+            curent_account = lst
+    while True:
+        existing_passcode = input(f"Enter passcode for username {username}: ")
+        if existing_passcode == "exit":
+            print("Login unssuccesful")
+            break
+        if existing_passcode == curent_account[1]:
+            print("Login successfull")
+            return True
+        else:
+            print("Wrong passcode!")
+            print("Try again or type 'exit'")
 
 
 def create_new_account(name, passcode, new_score=0):
@@ -142,7 +172,10 @@ if returning_player == "n":
     new_username = ask_for_username()
     new_passcode = ask_for_passcode()
     new_account = create_new_account(new_username, new_passcode)
-    acounts.append_row(new_account)
+    accounts.append_row(new_account)
+elif returning_player == "y":
+    old_user = log_in_username()
+    decision = log_in_passcode(old_user)
 
-print(new_account)
+print(decision)
 # main()
