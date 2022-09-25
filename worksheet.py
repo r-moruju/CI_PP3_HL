6f6343115bee5abe6e01e0f25362fa0752701e5a
R Moruju <morujurazvan@gmail.com>
2022-09-25 09:31:32 +0000

    Inplement 'print_logo' on the first part of the program

--- worksheet.py
+++ worksheet.py
@@ -1,3 +1,4 @@
+import time
 import gspread
 from google.oauth2.service_account import Credentials
 from colors import Color as col
@@ -23,15 +24,15 @@ def ask_if_returning_user():
     """
     Get input for new or returning user
     """
-    print(col.BLUE + "Welcome to Higher/Lower game".center(80))
+    print(col.BLUE + "Welcome to Higher/Lower game\n")
     while True:
-        question = input("Are you a returning user? (Y/N)\n ".center(80))
+        question = input("Are you a returning user? (Y/N)\n ")
         if question.lower() != "y" and question.lower() != "n":
             print_logo()
-            print("Type 'Y' for yes or 'N' for no".center(80))
+            print("Type 'Y' for yes or 'N' for no")
         else:
             print_logo()
-            print("Ok...".center(80))
+            print("Ok...")
             break
     return question.lower()
 
@@ -40,16 +41,21 @@ def ask_for_username():
     """
     Ask for username and check if is available
     """
-    print("Let's create a new account for you.\n")
+    print("Let's create a new account for you.")
+    time.sleep(3)
     keep_looping = True
     while keep_looping:
         keep_looping = False
+        print_logo()
         input_name = input("Type username: ")
         for lst in accounts_list:
             if input_name == lst[0]:
-                print("This username already in use. Try a different one.")
+                print("This username already in use. Try again.")
+                time.sleep(3.5)
                 keep_looping = True
+    print_logo()
     print(f"Welcome {input_name}")
+    time.sleep(3)
     return input_name
 
 
@@ -57,6 +63,7 @@ def ask_for_passcode():
     """
     Create new user pascode
     """
+    print_logo()
     passcode = input("Create your pascode: ")
     return passcode
 
@@ -66,15 +73,18 @@ def log_in_username():
     Ask user to login by entering username and passcode
     """
     while True:
+        print_logo()
         existing_username = input("Enter your username: ")
         if existing_username == "exit":
             print("\nLogin unssuccesful")
             return False
-        elif existing_username not in usernames:
-            print(f"\nThere is no account with username {existing_username}")
+        if existing_username not in usernames:
+            print(col.YELLOW + f"\nThere is no account with username {existing_username}")
             print("You can try again or type 'exit' to close")
+            time.sleep(4)
         else:
             print(f"\nWelcome back {existing_username}")
+            time.sleep(2)
             break
     return existing_username
 
@@ -87,24 +97,29 @@ def log_in_passcode(username):
         if lst[0] == username:
             curent_account = lst
     while True:
+        print_logo()
         existing_passcode = input(f"Enter passcode for username {username}: ")
         if existing_passcode == "exit":
-            print("Login unssuccesful")
+            print(col.GREEN + "Login unssuccesful")
+            time.sleep(3)
             break
         if existing_passcode == curent_account[1]:
-            print("Login successfull")
+            print(col.GREEN + "Login successfull")
+            time.sleep(3)
             return True
         else:
-            print("Wrong passcode!")
+            print(col.RED + "Wrong passcode!")
             print("Try again or type 'exit'")
+            time.sleep(3)
 
 
 def create_new_account(name, passcode, new_score=0):
     """
     Create a new account or update score
     """
+    print_logo()
     n_account = [name, passcode, new_score]
-    print("Account created successfuly.")
+    time.sleep(3)
     return n_account
 
 
@@ -112,7 +127,13 @@ def upload_new_acount(lst):
     """
     Update worksheet with new account
     """
-    accounts.append_row(lst)
+    try:
+        accounts.append_row(lst)
+    except ValueError as error:
+        print(f"{error} at account creation")
+        user_account_login()
+    else:
+        print(col.GREEN + "Account created successfuly.\n")
 
 
 def user_account_login():
